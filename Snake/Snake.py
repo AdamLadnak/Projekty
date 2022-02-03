f3d37a3d70232a0bd649a82964038d18a44d7c3a
AdamLadnak <a.ladnak25@gmail.com>
2022-02-03 14:51:58 +0100

    okno, had

--- Snake/Snake.py
+++ Snake/Snake.py
@@ -1,7 +1,7 @@
 from pyglet import app
 from pyglet import image
 from pyglet.window import Window
-#okno
+
 window = Window(750, 750)
 
 velkost_bunky = 25
@@ -9,17 +9,14 @@ velkost_bunky = 25
 snake_x = window.width // velkost_bunky // 2 * velkost_bunky
 snake_y = window.height // velkost_bunky // 2 * velkost_bunky
 
-
 @window.event
 def on_draw():
     window.clear()
-    nakresli_stvorec(snake_x, snake_y, velkost_bunky, colour = (255,0,0,0))
+    nakresli_stvorec(snake_x, snake_y, velkost_bunky, colour = (0,255,0,0))
 
 def nakresli_stvorec(x, y, size, colour = (255,255,255,0)):
     img = image.create(size,size,image.SolidColorImagePattern(colour))
     img.blit(x, y)
 
 
-
-#start hry
 app.run()
